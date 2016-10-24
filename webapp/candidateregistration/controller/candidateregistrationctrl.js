024831cb78eb36bc72a49ad3cf4746eb40aab8cf
kishanlalbj <kishanlalbj@gmail.com>
2016-10-24 15:09:57 +0000

    Candidate Registration redirects you to his profile

--- webapp/candidateregistration/controller/candidateregistrationctrl.js
+++ webapp/candidateregistration/controller/candidateregistrationctrl.js
@@ -1,11 +1,12 @@
-angular.module('samarth-coordinator').controller('candidateregistrationctrl',['$scope','registrationservice',
-	function($scope,registrationservice) {
+angular.module('samarth-coordinator').controller('candidateregistrationctrl',['$scope','registrationservice','$state',
+	function($scope,registrationservice,$state) {
 
-		
+		console.log("Inside candidate register ");
 		$scope.signup = function() {
 			console.log($scope.user);
 			registrationservice.registercandidate($scope.user).then(function(response) {
 				// console.log(response);
+				$state.go("verifyprofile",{candidateid: $scope.user.mobile});
 				$scope.signUpStatus = "Candidate Registered successfully";
 				console.log($scope.signUpstatus);
 			},function(err) {
